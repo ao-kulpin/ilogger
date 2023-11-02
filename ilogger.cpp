45f10a3ceef9e8a90fc188e1107f82834674fd38
Alexander Kulpin <alexander.kulpin7@gmail.com>
2023-11-02 11:28:01 +0300

    Initial/previous version (Windows)

---
+++ ilogger.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode >= 0) {
+        if (wParam == WM_KEYDOWN) {
+            KBDLLHOOKSTRUCT* kbdStruct = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+            DWORD keyCode = kbdStruct->vkCode;
+            std::cout << "Key press: " << keyCode << std::endl;
+        }
+        else if (wParam == WM_KEYUP) {
+            KBDLLHOOKSTRUCT* kbdStruct = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+            DWORD keyCode = kbdStruct->vkCode;
+            std::cout << "Key release: " << keyCode << std::endl;
+        }
+    }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+static int wheelDelta = 0;
+
+LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
+
+    if (nCode >= 0) {
+        if (wParam == WM_LBUTTONDOWN || wParam == WM_RBUTTONDOWN || wParam == WM_MBUTTONDOWN) {
+            switch (wParam) {
+            case WM_LBUTTONDOWN:
+                std::cout << "Mouse button press: LEFT" << std::endl;
+                break;
+            case WM_RBUTTONDOWN:
+                std::cout << "Mouse button press: RIGHT" << std::endl;
+                break;
+            case WM_MBUTTONDOWN:
+                std::cout << "Mouse button press: MIDDLE" << std::endl;
+                break;
+            }
+        }
+        else if (wParam == WM_LBUTTONUP || wParam == WM_RBUTTONUP || wParam == WM_MBUTTONUP) {
+            switch (wParam) {
+            case WM_LBUTTONUP:
+                std::cout << "Mouse button release: LEFT" << std::endl;
+                break;
+            case WM_RBUTTONUP:
+                std::cout << "Mouse button release: RIGHT" << std::endl;
+                break;
+            case WM_MBUTTONUP:
+                std::cout << "Mouse button release: MIDDLE" << std::endl;
+                break;
+            }
+        }
+        else if (wParam == WM_MOUSEWHEEL) {
+            MSLLHOOKSTRUCT* pMhs = (MSLLHOOKSTRUCT*)lParam;
+            short zDelta = HIWORD(pMhs->mouseData);
+            if(zDelta>0)
+                std::cout << "Mouse wheel: UP" << std::endl;
+            if(zDelta<0)
+                std::cout << "Mouse wheel: DOWN" << std::endl;
+        }
+        else if (wParam == WM_MOUSEMOVE) {
+            MSLLHOOKSTRUCT* mouseInfo = reinterpret_cast<MSLLHOOKSTRUCT*>(lParam);
+            int x = mouseInfo->pt.x;
+            int y = mouseInfo->pt.y;
+            std::cout << "Mouse move: X=" << x << ", Y=" << y << std::endl;
+        }
+    }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+int main() {
+    HHOOK keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, GetModuleHandle(NULL), 0);
+    HHOOK mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseProc, GetModuleHandle(NULL), 0);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(keyboardHook);
+    UnhookWindowsHookEx(mouseHook);
+
+    return 0;
+}

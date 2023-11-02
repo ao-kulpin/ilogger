a15b93e76ce9eb93a061554f1b925a0fe810baa7
Alexander Kulpin <alexander.kulpin7@gmail.com>
2023-11-02 18:31:50 +0300

    Placeholder for sender.ParseLine()

--- sender.cpp
+++ sender.cpp
@@ -4,10 +4,25 @@
 
 using namespace std;
 
+static bool ParseLine(const string line, INPUT& msg);
+
 DWORD WINAPI SenderThreadFunc(LPVOID param) {
 
-    while(true) {
-        cout << "Sender works ...\n";
-        Sleep(1000);
+    string line;
+    while(getline(cin, line)) {
+        cout << "\nSender got " << line << endl;
+        INPUT msg;
+        if (!ParseLine(line, msg)) {
+
+        }
+        Sleep(100);
     }
+
+    cout << "\nSender ended\n";
+
+}
+
+static bool ParseLine(const string line, INPUT& msg) {
+    memset(&msg, 0, sizeof msg);
+    return true;
 }

e7d377886429f32e2f01f3f5dd7fcae3deb0923f
Alexander Kulpin <alexander.kulpin7@gmail.com>
2023-11-03 17:09:52 +0300

    ParseLine() - mouse messages

--- sender.cpp
+++ sender.cpp
@@ -24,5 +24,55 @@ DWORD WINAPI SenderThreadFunc(LPVOID param) {
 
 static bool ParseLine(const string line, INPUT& msg) {
     memset(&msg, 0, sizeof msg);
+    auto& mi = msg.mi;
+
+    const char* linePtr = line.c_str();
+
+    char mousePressStr  [] = "Mouse button press:";
+    char mouseReleaseStr[] = "Mouse button release:";
+    char mouseWheelStr[]   = "Mouse Wheel:";
+    if (auto mousePressPtr = strstr(linePtr, mousePressStr)) {
+        msg.type = INPUT_MOUSE;
+        mousePressPtr += sizeof (mousePressStr) - 1;
+        if (strstr(mousePressPtr, " LEFT")) {
+            mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            return true;
+        } else if (strstr(mousePressPtr, " RIGHT")) {
+            mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+            return true;
+        } else if (strstr(mousePressPtr, " MIDDLE")) {
+            mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
+            return true;
+        } else
+            return false;
+    }
+    else if (auto mouseReleasePtr = strstr(linePtr, mouseReleaseStr)) {
+        msg.type = INPUT_MOUSE;
+        mouseReleasePtr += sizeof (mouseReleaseStr) - 1;
+        if (strstr(mouseReleasePtr, " LEFT")) {
+            mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            return true;
+        } else if (strstr(mouseReleasePtr, " RIGHT")) {
+            mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+            return true;
+        } else if (strstr(mouseReleasePtr, " MIDDLE")) {
+            mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
+            return true;
+        } else
+            return false;
+    } else if (auto mouseWheelPtr = strstr(linePtr, mouseWheelStr)) {
+        msg.type = INPUT_MOUSE;
+        mouseWheelPtr += sizeof (mouseWheelStr) - 1;
+        if (strstr(mouseWheelPtr, " UP")) {
+            mi.mouseData = WHEEL_DELTA;
+            return true;
+        } else if (strstr(mouseWheelPtr, " DOWN")) {
+            mi.mouseData = -WHEEL_DELTA;
+            return true;
+        } else
+            return false;
+    }
+    
+
     return true;
 }

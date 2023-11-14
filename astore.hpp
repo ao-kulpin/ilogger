6a709853331b3c0e6e4e1dd218fe7205c94d0e3d
Alexander Kulpin <alexander.kulpin7@gmail.com>
2023-11-14 19:55:26 +0300

    ActionStore (mouse actions)

--- astore.hpp
+++ astore.hpp
@@ -28,7 +28,41 @@ private:
     public:
         int     _nums[2] = {0, 0};
     };
+
+    class MCount {
+    public:
+        int     _nums[4][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+    };
+
     map<int, KCount> _kMap;         // keyboard actions
+
+    typedef unsigned long long ullong;
+    map<ullong, MCount> _mMap;       // mouse actions
+
+    inline
+    ullong getMouseKey(int x, int y) {
+        return (ullong(unsigned(x)) << 32) | ullong(unsigned(y));
+    }
+
+    inline
+    int& getCount(const MInput& mi, MCount& counters) {
+        switch(mi._action) {
+            case MInput::Action::press:
+            case MInput::Action::release:
+                return counters._nums[int(mi._action)][int(mi._button) - 1];
+
+            case MInput::Action::wheel:
+                return counters._nums[int(mi._action)][int(mi._wheelUp)];
+
+            case MInput::Action::move:
+                return counters._nums[int(mi._action)][0];
+
+            default:
+                assert(false);
+                return counters._nums[0][0];
+        }
+    }
 
 public:
     void put(const MKInput& mki, int mouseX, int MouseY) {
@@ -36,12 +70,13 @@ public:
 
         switch (mki._type) {
             case MKInput::Type::keyboard: {
-                auto& key = mki.mk._ki;
-                ++ _kMap[key._vk]._nums[int(key._action)];
+                auto& ki = mki.mk._ki;
+                ++ _kMap[ki._vk]._nums[int(ki._action)];
             }
             break;
 
-            case MKInput::Type::mouse:
+            case MKInput::Type::mouse: 
+                ++ getCount(mki.mk._mi, _mMap[getMouseKey(mouseX, MouseY)]);
                 break;
 
             default:
@@ -55,19 +90,27 @@ public:
 
         switch (mki._type) {
             case MKInput::Type::keyboard: {
-                auto& key = mki.mk._ki;
-                auto& count = _kMap[key._vk]._nums[int(key._action)];
-                if (count == 0)
+                auto& ki = mki.mk._ki;
+                auto& count = _kMap[ki._vk]._nums[int(ki._action)];
+                if (count <= 0)
                     return false;
                 else {
-                    ++ count;
+                    -- count;
                     return true;
                 }
             }
             break;
 
-            case MKInput::Type::mouse:
-                return false;
+            case MKInput::Type::mouse: {
+                auto& count = getCount(mki.mk._mi, _mMap[getMouseKey(mouseX, MouseY)]);
+                if (count <= 0)
+                    return false;
+                else {
+                    -- count;
+                    return true;
+                }
+            }
+            break;
 
             default:
                 assert(false);

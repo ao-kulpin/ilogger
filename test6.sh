ae085ddbaf38c7cfc32aa02c8ba3f959ba32075e
Alexander Kulpin <alexander.kulpin7@gmail.com>
2023-12-01 12:58:58 +0300

    test6.sh

---
+++ test6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+difflog() {
+	echo "ilogger cancelled by user. check logs..."
+	if [ ! -s tlk62.log ]; then
+		echo "test pass" 
+	else
+		echo "test fail"
+	fi		
+}
+
+trap "difflog" EXIT
+
+rm tlk62.log
+./ilogger < tlk61.log > tlk62.log --ownaction skip 
